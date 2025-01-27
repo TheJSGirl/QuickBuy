50ea516486832be90ec101ef95b0b747c7845932
Priyanka <prinegi100@gmail.com>
2025-01-27 17:28:08 +0530

    added dynamic navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink , Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf, NgSwitch, NgSwitchCase],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true
 })
 export class HeaderComponent {
+  menuType: String = 'default';
 
+  constructor(private route: Router){}
+
+  ngOnInit() : void{
+    this.route.events.subscribe((val: any) => {
+      console.log(val.url)
+      if(val.url) {
+        if(localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller'
+        }else {
+          this.menuType = 'default'
+        }
+      }
+    })
+
+  }
 }
