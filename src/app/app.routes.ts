8f525b81c6ce1c1cbaecdb9d1290f2b62f567a07
Priyanka <prinegi100@gmail.com>
2025-01-27 18:32:28 +0530

    added route for seller-add-product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 import { AuthGuard } from './auth.guard';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,11 @@ export const routes: Routes = [
     {
         path: 'seller-home',
         component: SellerHomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'seller-add-product',
+        component: SellerAddProductComponent,
         canActivate: [AuthGuard]
     }
 ];
